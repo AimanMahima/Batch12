79fa3b0e3b5679237f14bc3984861ef11f9c8f83
AimanMahima <aimanmahima018@gmail.com>
2020-02-12 10:52:38 +0600

    Routing

---
+++ resources/views/data.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Information</title>
+</head>
+<body>
+		<form>
+  <div class="form-group">
+    <label>Student Name</label><br>
+    <input type="name" class="form-control" id="name">
+  </div>
+  <div class="form-group">
+    <label>Roll</label><br>
+    <input type="roll" class="form-control" id="roll">
+  </div>
+  <div class="form-group">
+    <label>Fathers Name</label><br>
+    <input type="roll" class="form-control" id="roll">
+  </div>
+  <div class="form-group">
+    <label>Mothers Name</label><br>
+    <input type="roll" class="form-control" id="roll">
+  </div>
+  <div class="form-group">
+    <label>Institute</label><br>
+    <input type="roll" class="form-control" id="roll">
+  </div>
+  
+  
+  <button type="submit" class="btn btn-success">Submit</button>
+</form>
+</body>
+</html>
